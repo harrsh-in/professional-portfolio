9b8a78c333255e623d9753608dba04eb67047333
Harrsh Patel <dev@harrsh.com>
2023-12-09 16:14:49 +0530

    Added pages

---
+++ app/constants/metadata.ts
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+const CommonMetaData: Metadata = {
+    authors: [
+        {
+            name: "Harrsh Patel",
+        },
+    ],
+};
+
+export default CommonMetaData;
