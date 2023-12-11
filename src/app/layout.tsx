2583213f05d081047729211225acfd0c87e3958c
Harrsh Patel <dev@harrsh.com>
2023-12-11 13:33:44 +0530

    Added page loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 import "../globals.css";
 import { Inter } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
@@ -16,9 +18,28 @@ export default function RootLayout({
 }: {
     children: React.ReactNode;
 }) {
+    const [randomColor, setRandomColor] = useState<string>("#fff");
+
+    useEffect(() => {
+        const colors = [
+            "#1586EC",
+            "#1EAEC9",
+            "#FB3B08",
+            "#A847B5",
+            "#CE2B60",
+            "#F73E03",
+            "#F0EC03",
+        ];
+        setRandomColor(colors[Math.floor(Math.random() * colors.length)]);
+    }, []);
+
     return (
         <html lang="en">
             <body className={inter.className}>
+                <NextTopLoader
+                    color={randomColor}
+                    showSpinner={false}
+                />
                 <ScrollToTop />
 
                 <main className="mx-auto flex min-h-screen w-screen max-w-[600px] flex-col px-4">
