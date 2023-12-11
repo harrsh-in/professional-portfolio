14c9af9343773b8675f0c8451b16894bfb1df88e
Harrsh Patel <dev@harrsh.com>
2023-12-11 13:41:23 +0530

    Hide scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
 
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body className={`${inter.className} no-scrollbar`}>
                 <NextTopLoader
                     color={randomColor}
                     showSpinner={false}
